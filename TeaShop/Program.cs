2d9c28967013c847ebdf4b5fba47e17217fc9068
Gabriela Bloch <gabriela.bloch@microsoft.wsei.edu.pl>
2023-02-10 18:36:47 +0100

    Przygotwanie strony głównej

--- TeaShop/Program.cs
+++ TeaShop/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using TeaShop.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration["Data:DefaultConnection"]));
 builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddDirectoryBrowser();
 
 var app = builder.Build();
 
@@ -21,6 +23,19 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\images")),
+    RequestPath = new PathString("/images")
+});
+
+app.UseDirectoryBrowser(new DirectoryBrowserOptions()
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\images")),
+    RequestPath = new PathString("/images")
+});
 
 app.UseRouting();
 
